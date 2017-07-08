d33890e7433743633b53cada56dc661b9c9298c6
pranav <pran4ajith@gmail.com>
2017-07-08 08:59:57 +0530

    updated django

--- book_trial/sports_book/book/admin.py
+++ book_trial/sports_book/book/admin.py
@@ -1,10 +1,25 @@
+from __future__ import unicode_literals
 from django.contrib import admin
+
 
 # Register your models here.
 from .models import Admin_setup, Facility_master, Facility_availability
 
-admin.site.register(Admin_setup)
-admin.site.register(Facility_master)
-admin.site.register(Facility_availability)
+class Admin_setupAdmin(admin.ModelAdmin):
+    list_display=['name', 'emp_id']
+    
+class Facility_masterAdmin(admin.ModelAdmin):
+    list_display=['fac_id', 'fac_name', 'active']
+class Facility_availabilityAdmin(admin.ModelAdmin):
+    list_display=['fac_name', 'day', 'start_time', 'end_time']
+
+def my_function(self, fac_name):
+    """My Custom Title"""
+    my_function.short_description = 'proper'
+
+
+admin.site.register(Admin_setup, Admin_setupAdmin)
+admin.site.register(Facility_master, Facility_masterAdmin)
+admin.site.register(Facility_availability, Facility_availabilityAdmin)
 
 
--- book_trial/sports_book/book/models.py
+++ book_trial/sports_book/book/models.py
@@ -7,14 +7,23 @@ from django.db import models
 class Admin_setup(models.Model):
 	name = models.CharField(max_length=200)
 	emp_id = models.CharField(max_length=200)
-
+	class Meta:
+		verbose_name= 'Admin Setup'
+		verbose_name_plural='Admin Setup'
+    
 
 class Facility_master(models.Model):
-	fac_id = models.CharField(max_length=200)
-	fac_name = models.CharField(max_length=200)
-	active = models.BooleanField()
-	description = models.TextField()
-	image = models.FileField(blank = True)
+	fac_id = models.CharField(max_length=200, verbose_name="ID")
+	fac_name = models.CharField(max_length=200, verbose_name="Facility")#verbose_name for custom field name
+	active = models.BooleanField(verbose_name="Active")
+	description = models.TextField(verbose_name="Description")
+	image = models.FileField(blank = True, verbose_name="Image")
+	class Meta:
+		verbose_name= 'Add Facility'
+		verbose_name_plural='Add Facility'
+	def __unicode__(self):
+		return str(self.fac_name)
+
 	'''
 	In settings.py add the following for file upload
 
@@ -24,9 +33,30 @@ class Facility_master(models.Model):
 
 
 class Facility_availability(models.Model):
+    fac_name = models.ForeignKey("Facility_master")
+    #fac_id= models.
+    day = models.CharField(max_length=20, verbose_name="Day")
+    start_time = models.TimeField(auto_now=False, auto_now_add=False, verbose_name="Start Time")
+    end_time = models.TimeField(auto_now=False, auto_now_add=False, verbose_name="End Time")
+    class Meta:
+        verbose_name= 'Facility Availability'
+        verbose_name_plural='Facility Availability'
+
+'''class user(models.Model):
+	user= models.ForeignKey('auth.User', verbose_name="USER", blank=True, null=True)
+    first_name= models.CharField(verbose_name="First Name", max_length=30)
+    last_name= models.CharField(verbose_name="First Name", max_length=30)
+    emp_id= models.CharField(verbose_name="Employee Number", max_length=300)
+    email= models.EmailField(verbose_name="Email ID")
+    phone= models.CharField(verbose_name="Phone Number", blank=True)
+    class Meta:
+		verbose_name= 'Facility Availability'
+		verbose_name_plural='Facility Availability'
 	def __unicode__(self):
-		return unicode(self.fac_name)
-	fac_name = models.OneToOneField('Facility_master', on_delete=models.CASCADE)
-	day = models.CharField(max_length=20)
-	start_time = models.TimeField(auto_now=False, auto_now_add=False)
-	end_time = models.TimeField(auto_now=False, auto_now_add=False)
+		return str(self.user, self.emp_id)
+
+class booking(models.Model):
+    user_name= models.ForeignKey("user")
+    date_from= models.DateTimeField(verbose_name="From")
+    date_until=models.DateTimeField(verbose_name="Till")
+'''
