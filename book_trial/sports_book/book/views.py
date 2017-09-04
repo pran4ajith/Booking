b22c56d9f167bb5c6eff82450074a8f98c53b319
pranav <pran4ajith@gmail.com>
2017-09-04 10:33:51 +0530

    added comments

--- book_trial/sports_book/book/views.py
+++ book_trial/sports_book/book/views.py
@@ -11,11 +11,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 from book.models import Facility_master, Book_Facility,Facility_availability
 from django.contrib.auth import (
-	authenticate, 
-	login, 
-	get_user_model,
-	logout,
-	)
+    authenticate, 
+    login, 
+    get_user_model,
+    logout,
+    )
 from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
 from django.contrib.auth.decorators import login_required
 #add @login_required(login_url='/login/')
@@ -23,67 +23,67 @@ from .forms import UserLoginForm, UserRegisterForm, FacilityBookForm, EditProfil
 import datetime
 #home
 def index(request):
-	facilities =Facility_master.objects.exclude(active='False')#FILTERS OUT AND DISPLAY ONLY ACTIVE FACILITIES
-	return render(request, 'book/index.html', {
-		'facilities': facilities,
-		})
+    facilities =Facility_master.objects.exclude(active='False')#FILTERS OUT AND DISPLAY ONLY ACTIVE FACILITIES
+    return render(request, 'book/index.html', {
+        'facilities': facilities,
+        })
 
 #facility display
 def facility_detail(request, id):
-	try:
-		facility= Facility_master.objects.get(id=id)
-	except Facility_master.DoesNotExist:
-		raise Http404('The facility is not available')
-	return render(request, 'book/facility_detail.html', {
-		'facility':facility,
-		})
+    try:
+        facility= Facility_master.objects.get(id=id)
+    except Facility_master.DoesNotExist:
+        raise Http404('The facility is not available')
+    return render(request, 'book/facility_detail.html', {
+        'facility':facility,
+        })
 
 #login
 def login_view(request):
-	print request.user.is_authenticated()
-	title= "Login"
-	form = UserLoginForm(request.POST or None)
-	if form.is_valid():
-		username = form.cleaned_data.get("username")
-		password = form.cleaned_data.get("password")
-		user = authenticate(username=username, password=password)
-		if user is not None:
-			if user.is_active:
-				login(request, user)
-				'''
-				 if user is not None and user.is_active and user.is_staff:      
-						return redirect("/admin/")
-					#redirect
-					else:
-						'''
-				return redirect("/")
-	return render(request, "login_reg/login_form.html", {"form":form, "title":title})#no need for title though. did for some javascript. could have just added it directly to template.
+    print request.user.is_authenticated()
+    title= "Login"
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                '''
+                 if user is not None and user.is_active and user.is_staff:      
+                        return redirect("/admin/")
+                    #redirect
+                    else:
+                        '''
+                return redirect("/")
+    return render(request, "login_reg/login_form.html", {"form":form, "title":title})#no need for title though. did for some javascript. could have just added it directly to template.
 
 
 #register
 def register_view(request):
-	print request.user.is_authenticated()
-	title= "Register"
-	form= UserRegisterForm(request.POST or None)
-	if form.is_valid():
-		user = form.save(commit=False)
-		password = form.cleaned_data.get('password')
-		user.set_password(password)
-		user.save()
+    print request.user.is_authenticated()
+    title= "Register"
+    form= UserRegisterForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        password = form.cleaned_data.get('password')
+        user.set_password(password)
+        user.save()
 
-		new_user = authenticate(username= user.username, password=password)
-		login(request, new_user)
-		return redirect("/")
-		#return
+        new_user = authenticate(username= user.username, password=password)
+        login(request, new_user)
+        return redirect("/")
+        #return
 
-	return render(request, "login_reg/register_form.html", {"form":form, "title":title})
+    return render(request, "login_reg/register_form.html", {"form":form, "title":title})
 
 
 #logout
 @login_required(login_url='/login/')
 def logout_view(request):
-	logout(request)
-	return redirect("/")
+    logout(request)
+    return redirect("/")
 #facilility booking
 @login_required(login_url='/login/')
 def booking_view(request): 
@@ -105,41 +105,41 @@ def booking_view(request):
 #booking history
 @login_required(login_url='/login/')
 def history(request):
-	histories=Book_Facility.objects.filter(username=request.user)
-	return render(request, "book/history.html", {"histories":histories})
+    histories=Book_Facility.objects.filter(username=request.user)
+    return render(request, "book/history.html", {"histories":histories})
 
 @login_required(login_url='/login/')
 def book_new(request):
-	today=datetime.datetime.today()
-	bookings=Book_Facility.objects.filter(book_date=today)
-	return render(request, "book/bookings.html", {"bookings":bookings})
+    today=datetime.datetime.today()
+    bookings=Book_Facility.objects.filter(book_date=today)
+    return render(request, "book/bookings.html", {"bookings":bookings})
 #edit details
 
-class ProfileUpdate(LoginRequiredMixin, UpdateView):''' @loginrequired could only be used for functions. FOR CLASS loginrequiredmixin is used. needed to use a class heere for it. def did not work'''
-	login_url = '/login/'
-	model = User
-	form_class= EditProfileForm;
-	template_name = 'login_reg/edit_profile.html'
-	success_url = reverse_lazy('index') 
+class ProfileUpdate(LoginRequiredMixin, UpdateView):# @loginrequired could only be used for functions. FOR CLASS loginrequiredmixin is used. needed to use a class heere for it. def did not work.
+    login_url = '/login/'
+    model = User
+    form_class= EditProfileForm;
+    template_name = 'login_reg/edit_profile.html'
+    success_url = reverse_lazy('index') 
 
-	def get_object(self, queryset=None):
-		'''This method will load the object
-		   that will be used to load the form
-		   that will be edited'''
-		return self.request.user
+    def get_object(self, queryset=None):
+        '''This method will load the object
+           that will be used to load the form
+           that will be edited'''
+        return self.request.user
 
 @login_required(login_url='/login/')
 def change_password(request):
-	if request.method == 'POST':
-		form = PasswordChangeForm(request.user ,request.POST)
-		if form.is_valid():
-			user=form.save()
-			update_session_auth_hash(request, user)
-			messages.success(request, 'Your password was successfully updated!')
-			return redirect("/changepassword/")
-		else:
-			messages.error(request, 'Please correct the error below.')
-	else:
-		form = PasswordChangeForm(user =request.user)
-		   
-	return render(request, "book/change_password.html",{"form":form})
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user ,request.POST)
+        if form.is_valid():
+            user=form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect("/changepassword/")
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(user =request.user)
+           
+    return render(request, "book/change_password.html",{"form":form})
